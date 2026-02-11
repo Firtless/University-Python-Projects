38ba4f41a2a7d9143546968fc0cc2511baf84008
Firtless <tadeivybranets@gmail.com>
2026-02-11 10:56:03 +0200

    poting map list and their positions

---
+++ cs2push.py
@@ -0,0 +1,7 @@
+import random
+
+cs_maps = [Dust2, Nuke, Inferno, Mirrage]
+
+Dust2 = ["Mid", "A-site", "B-ste", "Short", "Long", "Lower"]
+
+Nuke = ["Ramp", "A-site", "B-ste", "Door", "Outside", "Vent", "Secret", ""]
