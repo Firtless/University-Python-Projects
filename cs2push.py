413d1aaa5311d3f38477c7c35b4c8de6a28dedc2
Firtless <tadeivybranets@gmail.com>
2026-02-11 11:10:19 +0200

    first= trial at ligic and relsuts

--- cs2push.py
+++ cs2push.py
@@ -1,7 +1,24 @@
 import random
 
+
+Dust2 = ["Mid", "A-site", "B-site", "Short", "Long", "Lower"]
+
+Nuke = ["Ramp", "A-site", "B-ste", "Door", "Outside", "Vent", "Secret", "Roof"]
+
+Inferno = ["Mid", "A-site", "B-ste", "Bannana", "Apps", "Arch"]
+
+Mirrage = ["Mid", "A-site", "B-ste", "Jungle", "Apps", "Lower", "Short"]
+
 cs_maps = [Dust2, Nuke, Inferno, Mirrage]
 
-Dust2 = ["Mid", "A-site", "B-ste", "Short", "Long", "Lower"]
+map = input("Enter your map: ")
 
-Nuke = ["Ramp", "A-site", "B-ste", "Door", "Outside", "Vent", "Secret", ""]
+for i in cs_maps:
+    if map == int(cs_maps[i]):
+        j = random.randint(1, len(map))
+    else:
+        print("Enter correctly")
+    i += 1
+
+if __name__ == "__main__":
+    print(map[j])
