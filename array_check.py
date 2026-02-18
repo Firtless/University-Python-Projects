60dc3a606c769320babb13506422349fa684eab2
Firtless <tadeivybranets@gmail.com>
2026-02-18 08:45:10 +0200

    New changes to correct the Test aspect

--- array_check.py
+++ array_check.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 def check_arr(arr):
 
     is_up = True
@@ -13,18 +10,3 @@ def check_arr(arr):
             is_down = False
 
     return is_up or is_down
-
-
-class TestCheckArr(unittest.TestCase):
-    def test_example_1(self):
-        self.assertTrue(check_arr([1, 2, 3, 4, 5]))
-
-    def test_example_2(self):
-        self.assertTrue(check_arr([5, 4, 3, 2, 1]))
-
-    def test_example_3(self):
-        self.assertFalse(check_arr([1, 2, 2, 3, 2, 4, 5]))
-
-
-if __name__ == '__main__':
-    unittest.main()
