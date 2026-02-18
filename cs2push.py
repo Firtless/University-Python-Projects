ef87c9a5968c985b2e10fd86d1bdb3c235d95cec
Firtless <tadeivybranets@gmail.com>
2026-02-18 10:47:57 +0200

    got random working but it cycles through all the lists not the chosen one

--- cs2push.py
+++ cs2push.py
@@ -11,10 +11,21 @@ Mirrage = ["Mid", "A-site", "B-ste", "Jungle", "Apps", "Lower", "Short"]
 
 cs_maps = [Dust2, Nuke, Inferno, Mirrage]
 
-map = input("Enter your map: ")
+# map = input("Enter your map: ")
 
-for i in cs_maps:
 
+def map_random(cs_maps):
+    random_sublist = random.choice(cs_maps)
+    random_item = random.choice(random_sublist)
+
+    print(f"random chose: {random_item}")
+
+
+# def cs_pick(cs_maps):
+#     if map == cs_maps:
+#         try:
+#             current_map = map_random
+#         except:
 
 if __name__ == "__main__":
-    map = input("Enter your map: ")
+    map_random(cs_maps)
