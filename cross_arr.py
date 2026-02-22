2f3d5b4490661620e637eabe71ac095de2a5eef2
Firtless <tadeivybranets@gmail.com>
2026-02-22 14:49:19 +0200

    Code clean up

--- cross_arr.py
+++ cross_arr.py
@@ -4,8 +4,13 @@ arr_B = [2, 4, 6, 7]
 
 cross = []
 
-for item in arr_A:
-    if item in arr_B:
-        cross.append(item)
+
+def task1(cross):
+    for item in arr_A:
+        if item in arr_B:
+            cross.append(item)
+
+
+task1(cross)
 
 print(f"common items: {cross}")
