e1860ffd5c0e31fe019ddfef8c7375e202d2875f
Firtless <tadeivybranets@gmail.com>
2026-03-03 21:41:58 +0200

    completed a task for Binary Search in the search of optimal board space for sticky notes

--- lab2.py
+++ lab2.py
@@ -1,1 +1,16 @@
-def min_board_space():
+def min_board_space(n, w, h):
+    min_num = 1
+    max_num = max(w, h) * n
+    result = max_num
+
+    while min_num <= max_num:
+        avr = (min_num + max_num) // 2
+        count = (avr // w) * (avr // h)
+
+        if count >= n:
+            result = avr
+            max_num = avr - 1
+        else:
+            min_num = avr + 1
+
+    return result
