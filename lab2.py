4b1bdca8335f368a996278001ef623b314f4d837
Firtless <tadeivybranets@gmail.com>
2026-03-04 11:03:33 +0200

    New task + overwork

--- lab2.py
+++ lab2.py
@@ -1,9 +1,18 @@
+import math
+
+
 def min_board_space(n, w, h):
-    min_num = 1
+    min_num = max(w, h) - 1
     max_num = max(w, h) * n
+
     result = max_num
 
+    steps = 0
+
     while min_num <= max_num:
+
+        steps += 1
+
         avr = (min_num + max_num) // 2
         count = (avr // w) * (avr // h)
 
@@ -13,4 +22,10 @@ def min_board_space(n, w, h):
         else:
             min_num = avr + 1
 
+    print(f'the amount of steps: {steps}')
+
     return result
+
+
+# min1 = min_board_space(10000, 2, 3)
+# print(min1)
