5558a4fc73dfa4d2a2e44e120a2e969ef4d6309e
Firtless <tadeivybranets@gmail.com>
2026-03-10 23:12:41 +0200

    Clean up in function names

--- lab2.py
+++ lab2.py
@@ -2,21 +2,21 @@ import math
 
 
 def min_board_space(n, w, h):
-    side = max(math.isqrt(n * w * h), max(w, h))
+    board_side = max(math.isqrt(n * w * h), max(w, h))
 
     steps = 0
 
     while True:
         steps += 1
-        count = (side // w) * (side // h)
+        count = (board_side // w) * (board_side // h)
 
         if count >= n:
-            if ((side - 1) // w) * ((side - 1) // h) < n:
+            if ((board_side - 1) // w) * ((board_side - 1) // h) < n:
                 break
-            side -= 1
+            board_side -= 1
         else:
-            needed_ratio = math.sqrt(n // max(count, 1))
-            side = max(side + 1, int(side * needed_ratio))
+            perfect_ratio = math.sqrt(n // max(count, 1))
+            board_side = max(board_side + 1, int(board_side * perfect_ratio))
 
     print(f'the amount of steps: {steps}')
-    return side
+    return board_side
