b711e5e174c9e105f837231354f1fe93be2f70e0
Firtless <tadeivybranets@gmail.com>
2026-04-07 20:20:56 +0300

    Logic iteration for both output and input

---
+++ lab_test5.py
@@ -0,0 +1,22 @@
+import unittest
+from lab5 import unsorted_range
+
+
+class TestSubarrayLogic(unittest.TestCase):
+
+    def test_already_sorted(self):
+        self.assertEqual(unsorted_range([1, 2, 3, 4, 5]), (-1, -1))
+
+    def test_entire_array_needs_sorting(self):
+        self.assertEqual(unsorted_range([5, 4, 3, 2, 1]), (0, 4))
+
+    def test_single_element(self):
+        self.assertEqual(unsorted_range([10]), (-1, -1))
+
+    def test_provided_example(self):
+        arr = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+        self.assertEqual(unsorted_range(arr), (3, 9))
+
+
+if __name__ == '__main__':
+    unittest.main()
